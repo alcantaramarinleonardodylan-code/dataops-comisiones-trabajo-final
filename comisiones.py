c5a7edc26ad5f950cb8ee204c86aa55bf52da779
alcantaramarinleonardodylan-code <alcantaramarinleonardodylan@gmail.com>
2026-05-12 00:32:39 -0500

    Update comisiones.py

--- comisiones.py
+++ comisiones.py
@@ -1,13 +1,6 @@
 import pandas as pd
 
-datos = {
-    "Empleado": ["Juan", "Ana", "Luis"],
-    "Ventas": [1000, 1500, 2000]
-}
-
-df = pd.DataFrame(datos)
-
-df["Comision"] = df["Ventas"] * 0.10
+df = pd.read_csv("ComisionEmpleados_V1_202605.csv")
 
 df.to_excel("comisiones.xlsx", index=False)
 
