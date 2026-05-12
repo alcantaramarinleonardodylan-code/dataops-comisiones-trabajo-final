46a0b1a54658cff9a1f875e5f858aa13d78f4a02
alcantaramarinleonardodylan-code <alcantaramarinleonardodylan@gmail.com>
2026-05-12 00:45:19 -0500

    Update comisiones.py

--- comisiones.py
+++ comisiones.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("ComisionEmpleados_V1_202605.csv")
+df = pd.read_csv("ComisionEmpleados_V1_202605.csv", sep=";")
 
 df.to_excel("comisiones.xlsx", index=False)
 
